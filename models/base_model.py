199697fe312b850839572e3b6f3513e501cb6d29
Quinto-A <acquinoquinto5@gmail.com>
2025-02-25 00:11:38 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -20,14 +20,13 @@ class BaseModel:
             kwargs: dict of key-values arguments
         """
 
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                            kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                            kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ["created_at", "updated_at"]:
+                    setattr(self, key, datetime.strptime(value, 
+                        "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
